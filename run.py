313f04598b47aab4596093f4d788cb87e06f5319
Yaodong Zhao <git@yaodong.org>
2019-01-28 15:55:55 -0700

    first commit

---
+++ run.py
@@ -0,0 +1,9 @@
+from mappercore import Server
+
+# create server instance
+server = Server()
+
+# register your backend function here
+
+# to run the server, please use:
+#  gunicorn -w 4 run:server
